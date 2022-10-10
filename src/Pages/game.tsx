21d9c72df21bc6a3245a1d70609e838c56e5202a
Tarny94 <102902000+Tarny94@users.noreply.github.com>
2022-10-10 21:55:32 +0300

    create app X and 0

---
+++ src/Pages/game.tsx
@@ -0,0 +1,146 @@
+import { isDisabled } from "@testing-library/user-event/dist/utils";
+import React, { useState } from "react";
+import "../style/styles.scss";
+
+const Game = () => {
+  const [buttonOne, setButtonOne] = useState("😁");
+  const [buttonTwo, setButtonTwo] = useState("🌞");
+  const [buttonThree, setButtonThree] = useState("😎");
+  const [buttonFour, setButtonFour] = useState("😉");
+  const [buttonFive, setButtonFive] = useState("😊");
+  const [buttonSix, setButtonSix] = useState("😋");
+  const [buttonSeven, setButtonSeven] = useState("😏");
+  const [buttonEight, setButtonEight] = useState("😧");
+  const [buttonNine, setButtonNine] = useState("😯");
+
+  const [istrue, setIstrue] = useState(true);
+  const [disable, setDisable] = useState(false);
+
+  const x = "❌";
+  const zero = "⭕";
+
+  const checkIfClicked = (button: any) => {
+    if (button === x || button === zero) {
+      return true;
+    }
+    return false;
+  };
+
+  const handleButton = (button: any) => {
+    if (istrue) {
+      button(x);
+      setIstrue(false);
+    } else {
+      button(zero);
+      setIstrue(true);
+    }
+  };
+
+  return (
+    <div className="App">
+      <div className="container">
+        <div
+          className="button button1"
+          onClick={() => {
+            if (checkIfClicked(buttonOne)) {
+              return "";
+            }
+            handleButton(setButtonOne);
+          }}
+        >
+          {buttonOne}
+        </div>
+        <div
+          className="button button2"
+          onClick={() => {
+            if (checkIfClicked(buttonTwo)) {
+              return "";
+            }
+            handleButton(setButtonTwo);
+          }}
+        >
+          {buttonTwo}
+        </div>
+        <div
+          className="button button3"
+          onClick={() => {
+            if (checkIfClicked(buttonThree)) {
+              return "";
+            }
+            handleButton(setButtonThree);
+          }}
+        >
+          {buttonThree}
+        </div>
+        <div
+          className="button button4"
+          onClick={() => {
+            if (checkIfClicked(buttonFour)) {
+              return "";
+            }
+            handleButton(setButtonFour);
+          }}
+        >
+          {buttonFour}
+        </div>
+        <div
+          className="button button5"
+          onClick={() => {
+            if (checkIfClicked(buttonFive)) {
+              return "";
+            }
+            handleButton(setButtonFive);
+          }}
+        >
+          {buttonFive}
+        </div>
+        <div
+          className="button button6"
+          onClick={() => {
+            if (checkIfClicked(buttonSix)) {
+              return "";
+            }
+            handleButton(setButtonSix);
+          }}
+        >
+          {buttonSix}
+        </div>
+        <div
+          className="button button7"
+          onClick={() => {
+            if (checkIfClicked(buttonSeven)) {
+              return "";
+            }
+            handleButton(setButtonSeven);
+          }}
+        >
+          {buttonSeven}
+        </div>
+        <div
+          className="button button8"
+          onClick={() => {
+            if (checkIfClicked(buttonEight)) {
+              return "";
+            }
+            handleButton(setButtonEight);
+          }}
+        >
+          {buttonEight}
+        </div>
+        <div
+          className="button button9"
+          onClick={() => {
+            if (checkIfClicked(buttonNine)) {
+              return "";
+            }
+            handleButton(setButtonNine);
+          }}
+        >
+          {buttonNine}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Game;
